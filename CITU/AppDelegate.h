0c6fc12b866e43c38cec166da5b8029e2d2829c9
GRGuys <centrin@fdh-1270.local>
2016-06-06 17:05:08 +0800

    Initial Commit

---
+++ CITU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CITU
+//
+//  Created by centrin on 16/6/6.
+//  Copyright © 2016年 CT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CITU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CITU
+//
+//  Created by centrin on 16/6/6.
+//  Copyright © 2016年 CT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
